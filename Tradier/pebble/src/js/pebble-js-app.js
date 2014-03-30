28bee4889d1e408077c8bcbf8362fc3b6044f7d7
Utsav Shah <ukshah2@illinois.edu>
2014-03-29 20:02:13 -0400

    Pebble code

---
+++ Tradier/pebble/src/js/pebble-js-app.js
@@ -0,0 +1,12 @@
+{
+  "uuid": "ca0a8c62-b790-11e3-9f3b-425861b86ab6",
+  "shortName": "Pebble Weather",
+  "longName": "Pebble Weather",
+  "companyName": "Pebble Technology",
+  "versionCode": 2,
+  "versionLabel": "2.0.0",
+  "watchapp": {
+    "watchface": false
+  },
+  "appKeys": {}
+  }
